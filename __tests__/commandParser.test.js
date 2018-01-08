67694a27766de8c9f2addfcab6715fc8a074f204
ddrgis <kasabiankpp@gmail.com>
2018-01-08 17:34:52 +0200

    Fix code duplication in tests

--- __tests__/commandParser.test.js
+++ __tests__/commandParser.test.js
@@ -1,25 +1,21 @@
 /* eslint-disable */
 import { getCommandParser } from '../src/commandParser';
 
+const parser = getCommandParser();
+const options = parser.options;
+
 test('CommandParser is returned as object', () => {
-  expect(typeof getCommandParser()).toBe('object');
+  expect(typeof parser).toBe('object');
 });
 
 test('CommandParser defines -f option', () => {
-  const parser = getCommandParser();
-  const options = parser.options;
-
   expect(options.find(opt => opt.short === '-f').short).toBe('-f');
 });
 
 test('CommandParser defines -V option', () => {
-  const parser = getCommandParser();
-  const options = parser.options;
-
   expect(options.find(opt => opt.short === '-V').short).toBe('-V');
 });
 
 test('CommandParser defines program description', () => {
-  const parser = getCommandParser();
   expect(parser._description).toBe('Compares two configuration files and shows a difference.');
 });
