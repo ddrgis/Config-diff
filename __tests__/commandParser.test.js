d7d59fde0a4a3a39ce06072fabd2107e0b0132d3
ddrgis <kasabiankpp@gmail.com>
2018-01-08 13:42:10 +0200

    Rename commandParser's method. Define 3 options and version

--- __tests__/commandParser.test.js
+++ __tests__/commandParser.test.js
@@ -1,6 +1,27 @@
 /* eslint-disable */
-import parse from '../src/commandParser';
+import { getCommandParser } from '../src/commandParser';
 
-test('Parsed result is object', () => {
-  expect(typeof parse()).toBe('object');
+test('CommandParser is returned as object', () => {
+  expect(typeof getCommandParser()).toBe('object');
+});
+
+test('CommandParser defines -f option', () => {
+  const parser = getCommandParser();
+  const options = parser.options;
+
+  expect(options.find(opt => opt.short === '-f').short).toBe('-f');
+});
+
+test('CommandParser defines -h option', () => {
+  const parser = getCommandParser();
+  const options = parser.options;
+
+  expect(options.find(opt => opt.short === '-h').short).toBe('-h');
+});
+
+test('CommandParser defines -V option', () => {
+  const parser = getCommandParser();
+  const options = parser.options;
+
+  expect(options.find(opt => opt.short === '-V').short).toBe('-V');
 });
--- src/commandParser.js
+++ src/commandParser.js
@@ -1,6 +1,12 @@
 import commander from 'commander';
 
-export default () => {
-  console.log(commander);
-  return commander;
+export const getCommandParser = () => {
+  const command = commander
+    .version('0.0.1')
+    .option('-f, --format [type]', 'Output format')
+    .option('-V, --version', 'output the version number')
+    .option('-h, --help', 'output usage information');
+  return command;
 };
+
+export default getCommandParser;
