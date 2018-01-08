702588c54027cabef82c946d6d1aaef1151cbaa3
ddrgis <kasabiankpp@gmail.com>
2018-01-08 17:30:08 +0200

    Add to travic CI

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 GenDiffProject
+[![Maintainability](https://api.codeclimate.com/v1/badges/37341afa0ee22f952a0e/maintainability)](https://codeclimate.com/github/ddrgis/project-lvl1-s212/maintainability) [![Build Status](https://travis-ci.org/ddrgis/project-lvl2-s213.svg?branch=master)](https://travis-ci.org/ddrgis/project-lvl2-s213)
--- src/commandParser.js
+++ src/commandParser.js
@@ -1,9 +1,10 @@
 import commander from 'commander';
+import info from '../package.json';
 
 export const getCommandParser = () => {
   const parser = commander
     .description('Compares two configuration files and shows a difference.')
-    .version('0.0.4')
+    .version(info.version)
     .option('-f, --format [type]', 'Output format')
     .option('-V, --version', 'output the version number')
     .arguments('<firstConfig> <secondConfig>');
