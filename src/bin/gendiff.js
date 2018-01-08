74eb2baac4e38e360df0ad10ace61042706a5531
ddrgis <kasabiankpp@gmail.com>
2018-01-08 15:17:39 +0200

    Fix ../index import

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import getParser from '../commandParser';
-import start from '../index';
+import start from '..';
 
 const cliCommandParser = getParser();
 const command = cliCommandParser.parse(process.argv);
