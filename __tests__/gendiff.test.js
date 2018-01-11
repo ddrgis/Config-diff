da91ddfb4929d96c4711b034d00c690063718be3
ddrgis <kasabiankpp@gmail.com>
2018-01-11 21:50:21 +0200

    Add jest to eslint config. Clean gendiff tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,39 +1,31 @@
-/* eslint-disable */
 import fs from 'fs';
 import path from 'path';
-//import parseCommand from '../src/commandParser';
 import genDiff from '../src/gendiff';
 
-const readFixtureFile = (fileName) => {
-  return fs.readFileSync(path.join(__dirname, `./__fixtures__/${fileName}`), 'utf-8').replace(/"/g, '');
-};
+const readFixtureFile = fileName => fs.readFileSync(path.join(__dirname, `./__fixtures__/${fileName}`), 'utf-8').replace(/"/g, '');
 
-const getPathToFixtures = (fileName) => {
-  return `./__tests__/__fixtures__/${fileName}`;
-};
+const getPathToFixtures = fileName => `./__tests__/__fixtures__/${fileName}`;
 
-const testInit = (inputFormat) => {
-  return {
-    beforeConfig: getPathToFixtures(`before.${inputFormat}`),
-    afterConfig: getPathToFixtures(`after.${inputFormat}`),
-    resultDiff: readFixtureFile('diff.json'),
-  }
-}
+const testInit = inputFormat => ({
+  beforeConfig: getPathToFixtures(`before.${inputFormat}`),
+  afterConfig: getPathToFixtures(`after.${inputFormat}`),
+  resultDiff: readFixtureFile('diff.json'),
+});
 
 test('correct json input returns corrent diff json', () => {
-  const {beforeConfig, afterConfig, resultDiff} = testInit('json');
+  const { beforeConfig, afterConfig, resultDiff } = testInit('json');
 
   expect(genDiff(beforeConfig, afterConfig)).toEqual(resultDiff);
 });
 
 test('correct yaml input returns corrent diff json', () => {
-  const {beforeConfig, afterConfig, resultDiff} = testInit('yaml');
+  const { beforeConfig, afterConfig, resultDiff } = testInit('yaml');
 
   expect(genDiff(beforeConfig, afterConfig)).toEqual(resultDiff);
 });
 
 test('correct ini input returns corrent diff json', () => {
-  const {beforeConfig, afterConfig, resultDiff} = testInit('ini');
+  const { beforeConfig, afterConfig, resultDiff } = testInit('ini');
 
   expect(genDiff(beforeConfig, afterConfig)).toEqual(resultDiff);
 });
