721f4905e9f8051047ffdd5bc05ece869de64d8e
ddrgis <kasabiankpp@gmail.com>
2018-01-12 06:15:37 +0200

    Implementer makeDiff for flat configs using AST

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,6 +3,8 @@ import path from 'path';
 import _ from 'lodash';
 import yamlParser from 'js-yaml';
 import iniParser from 'ini';
+import { create } from 'domain';
+import { start } from 'repl';
 
 const readFile = pathToFile => fs.readFileSync(path.resolve(pathToFile), 'utf-8');
 
@@ -28,24 +30,39 @@ const getParseMethod = (configPath) => {
   return parsers[configExtention].parse;
 };
 
-export const makeDiff = (before, after) => {
-  const diffExpectNewLines = _.reduce(before, (acc, beforeValue, key) => {
-    const afterValue = after[key];
-    if (afterValue === beforeValue) {
-      return { ...acc, [`  ${key}`]: beforeValue };
-    }
-    if (afterValue === undefined) {
-      return { ...acc, [`- ${key}`]: beforeValue };
-    }
-    return { ...acc, [`+ ${key}`]: afterValue, [`- ${key}`]: beforeValue };
+const createAST = (object) => {
+  return _.reduce(object, (acc, value, key) => {
+    return { ...acc, [key]: { value, status: null, children: {} } };
   }, {});
+};
 
-  const fullDiff = _.reduce(
-    after,
-    (acc, afterValue, key) => (before[key] === undefined ? { ...acc, [`+ ${key}`]: afterValue } : acc),
-    diffExpectNewLines,
-  );
-  return JSON.stringify(fullDiff, undefined, '  ').replace(/"/g, '');
+const diffAST = (beforeChangingAST, afterChangingAST) => {
+  const withoutAddedLines = _.reduce(beforeChangingAST, (acc, before, key) => {
+    const after = afterChangingAST[key];
+    if (after === undefined) {
+      return { ...acc, [`- ${key}`]: { ...before, status: 'removed' } };
+    }
+    if (after.value === before.value) {
+      return { ...acc, [`  ${key}`]: { ...before, status: 'notChanged' } };
+    }
+    return { ...acc, [`+ ${key}`]: { ...after, status: 'Changed' }, [`- ${key}`]: { ...before, status: 'changed' } };
+  }, {});
+  const addedKeys = Object.keys(afterChangingAST)
+    .filter(key => beforeChangingAST[key] === undefined);
+
+  return _.reduce(addedKeys, (acc, key) => ({ ...acc, [`+ ${key}`]: { ...afterChangingAST[key], status: 'added' } }), withoutAddedLines);
+};
+
+const diffASTToString = ast => JSON.stringify(
+  _.reduce(ast, (acc, value, key) => ({ ...acc, [key]: value.value }), {}),
+  undefined, '  ',
+).replace(/"/g, '');
+
+export const makeDiff = (before, after) => {
+  const beforeChangingAST = createAST(before);
+  const afterChangingAST = createAST(after);
+  const diff = diffAST(beforeChangingAST, afterChangingAST);
+  return diffASTToString(diff);
 };
 
 export const genDiff = (firstConfigPath, secondConfigPath) => {
