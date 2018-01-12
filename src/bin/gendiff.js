ccdfb3bb7b43b44d7db5ba1b9eeac7d1318ca059
ddrgis <kasabiankpp@gmail.com>
2018-01-13 00:28:03 +0200

    Move commandParser to gendiff bin. Fix cli arguments handling

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,7 +1,27 @@
 #!/usr/bin/env node
-import parseCommand from '../commandParser';
+import commander from 'commander';
+import info from '../../package.json';
 import genDiff from '..';
 
-const command = parseCommand(process.argv);
-const [firstConfigPath, secondConfigPath] = command.args;
-console.log(genDiff(firstConfigPath, secondConfigPath));
+commander
+  .description('Compares two configuration files and shows a difference.')
+  .version(info.version)
+  .option('-V, --version', 'output the version number');
+
+commander
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format', 'json')
+  .action((firstConfigPath, secondConfigPath, cmd) => {
+    if (cmd.format !== 'json') {
+      console.log('Different formats haven\'t impletemented yet\nUse gendiff <firstConfigPath> <secondConfigPath>');
+    } else {
+      console.log(genDiff(firstConfigPath, secondConfigPath));
+    }
+  });
+
+commander.parse(process.argv);
+
+const isNoCommandSpecified = commander.args.length === 0;
+if (isNoCommandSpecified) {
+  commander.help();
+}
