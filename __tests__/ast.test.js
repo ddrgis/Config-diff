845e22ea0b17b53635c67f88f06ca9c54ddbd869
ddrgis <kasabiankpp@gmail.com>
2018-01-14 10:41:57 +0200

    Add test with big treelike ast parsing

--- __tests__/ast.test.js
+++ __tests__/ast.test.js
@@ -30,7 +30,7 @@ test('flat ast rendering to json', () => {
   expect(render(flatAST)).toBe(expectedResult);
 });
 
-const treelikeAST = [
+const simpleTreelikeAST = [
   {
     name: 'group1',
     type: 'internalNode',
@@ -49,5 +49,63 @@ test('simple treelike AST parsing (group1)', () => {
   const firstConfig = parseConfig(getPathToFixtures('treelikeConfigs/group1Before.json'));
   const secondConfig = parseConfig(getPathToFixtures('treelikeConfigs/group1After.json'));
 
-  expect(parseAST(firstConfig, secondConfig)).toEqual(treelikeAST);
+  expect(parseAST(firstConfig, secondConfig)).toEqual(simpleTreelikeAST);
+});
+
+const bigTreelikeAST = [
+  {
+    name: 'common',
+    type: 'internalNode',
+    children: [
+      { name: 'setting1', type: 'notChanged', newValue: 'Value 1' },
+      { name: 'setting2', type: 'deleted', previousValue: '200' },
+      { name: 'setting3', type: 'notChanged', newValue: true },
+      {
+        name: 'setting6',
+        type: 'internalNode',
+        children: [
+          { name: 'key', type: 'notChanged', newValue: 'value' },
+          { name: 'ops', type: 'added', newValue: 'vops' },
+        ],
+      },
+      { name: 'setting4', type: 'added', newValue: 'blah blah' },
+      {
+        name: 'setting5', type: 'added', newValue: { key5: 'value5' },
+      },
+    ],
+  },
+  {
+    name: 'group1',
+    type: 'internalNode',
+    children: [
+      {
+        name: 'baz',
+        type: 'changed',
+        newValue: 'bars',
+        previousValue: 'bas',
+      },
+      {
+        name: 'foo',
+        type: 'notChanged',
+        newValue: 'bar',
+      },
+    ],
+  },
+  {
+    name: 'group2',
+    type: 'deleted',
+    previousValue: { abc: '12345' },
+  },
+  {
+    name: 'group3',
+    type: 'added',
+    newValue: { fee: '100500' },
+  },
+];
+
+test('treelike ast parsing', () => {
+  const firstConfig = parseConfig(getPathToFixtures('treelikeConfigs/before.json'));
+  const secondConfig = parseConfig(getPathToFixtures('treelikeConfigs/after.json'));
+
+  expect(parseAST(firstConfig, secondConfig)).toEqual(bigTreelikeAST);
 });
