ec50ea714345195f21d8d66d4379231cf49c84e7
ddrgis <kasabiankpp@gmail.com>
2018-01-14 09:25:55 +0200

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 
-import buildAST, { render as diffASTToString } from './ast';
+import buildAST, { render as astToString } from './ast';
 import parse from './parser';
 
-const makeDiff = (firstConfig, secondConfig) => {
-  const diffAST = buildAST(firstConfig, secondConfig);
-  return diffASTToString(diffAST);
+// Оставил эту функцию для возможной адаптации к данным не из файлов
+const genDiffFromData = (firstConfigData, secondConfigData) => {
+  const diffAST = buildAST(firstConfigData, secondConfigData);
+  return astToString(diffAST);
 };
 
 const genDiff = (firstConfigPath, secondConfigPath) => {
-  const firstConfig = parse(firstConfigPath);
-  console.log('firstConfig: ', firstConfig);
-  const secondConfig = parse(secondConfigPath);
-  return makeDiff(firstConfig, secondConfig);
+  const firstConfigData = parse(firstConfigPath);
+  const secondConfigData = parse(secondConfigPath);
+  return genDiffFromData(firstConfigData, secondConfigData);
 };
 
 export default genDiff;
