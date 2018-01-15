71d12050c96c51caf7fd12b27071c4370812ab84
ddrgis <kasabiankpp@gmail.com>
2018-01-15 22:14:30 +0200

    Refactoring toJSONRender

--- src/renders/toJSONRender.js
+++ src/renders/toJSONRender.js
@@ -1,11 +1,9 @@
 const nodeTypes = {
   internalNode: ({ name, children }, toJSONFunc) => ({ [name]: toJSONFunc(children) }),
-  deleted: ({ name, type, oldValue }) => ({ [name]: { was: type, oldValue } }),
-  added: ({ name, type, newValue }) => ({ [name]: { was: type, newValue } }),
-  notChanged: ({ name, type, newValue }) => ({ [name]: { was: type, value: newValue } }),
-  changed: ({
-    name, type, oldValue, newValue,
-  }) => ({ [name]: { was: type, from: oldValue, to: newValue } }),
+  deleted: ({ oldValue, ...node }) => ({ [node.name]: { was: node.type, oldValue } }),
+  added: ({ newValue, ...node }) => ({ [node.name]: { was: node.type, newValue } }),
+  notChanged: ({ newValue, ...node }) => ({ [node.name]: { was: node.type, value: newValue } }),
+  changed: node => ({ [node.name]: { was: node.type, from: node.oldValue, to: node.newValue } }),
 };
 
 const toJSON = (ast) => {
